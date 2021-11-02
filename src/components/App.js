14768800f0d16fc15c8b79fa2461e95449f2db21
Rachel Jensen <rachelangela@gmail.com>
2021-11-02 16:07:53 -0600

    Add component files

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,15 @@
-import logo from './logo.svg';
+
 import './App.css';
 
+//process.env.REACT_APP_API_KEY
+
 function App() {
+  
+
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
