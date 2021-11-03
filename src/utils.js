2119a2baae2566b214b5c6bea43760091aba8eb6
Rachel Jensen <rachelangela@gmail.com>
2021-11-02 19:21:10 -0600

    Remove unnecessary space

--- src/utils.js
+++ src/utils.js
@@ -1,4 +1,3 @@
-export const categories = ['arts', 'automobiles', 'books', 'business', 'fashion', 'food', 'health', 'home', 'insider', 'magazine', 'movies', 'nyregion', 'obituaries', 'opinion', 'politics', 'realestate', 'science', 'sports', 'sundayreview', 'technology', 'theater', 't-magazine', 'travel', 'upshot', 'us', 'world'
-]
+export const categories = ['arts', 'automobiles', 'books', 'business', 'fashion', 'food', 'health', 'home', 'insider', 'magazine', 'movies', 'nyregion', 'obituaries', 'opinion', 'politics', 'realestate', 'science', 'sports', 'sundayreview', 'technology', 'theater', 't-magazine', 'travel', 'upshot', 'us', 'world']
 
 export const categoriesEng = ['Arts', 'Automobiles', 'Books', 'Business', 'Fashion', 'Food', 'Health', 'Front Page', 'Insider', 'Magazine', 'Movies', 'N.Y. Region', 'Obituaries', 'Opinion', 'Politics', 'Real Estate', 'Science', 'Sports', 'Sunday Review', 'Technology', 'Theater', 'Times Magazine', 'Travel', 'Upshot', 'U.S.', 'World']
