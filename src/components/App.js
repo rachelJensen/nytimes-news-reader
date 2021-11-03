61f640cd91f0e40314a00e356175bb1a27bc7fa5
Rachel Jensen <rachelangela@gmail.com>
2021-11-02 19:23:16 -0600

    Add useEffect to get data for Headlines

--- src/components/App.js
+++ src/components/App.js
@@ -1,22 +1,30 @@
 import './App.css';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { getStories } from '../apiCalls.js';
 import Search from './Search'
+import Headlines from './Headlines';
 
 
 
 function App() {
   // keep subject in App state so navigating between headlines and detailed view doesn't reset the search
   const [category, setCategory] = useState('home');
-  const [stories, setStories] = useState([])
+  const [categoryHead, setCategoryHead] = useState('');
+  const [stories, setStories] = useState([]);
+
+  useEffect(() => {
+    setFromSearch(category)
+  }, [])
 
   const setFromSearch = (topic) => {
     setCategory(topic);
     getStories(topic)
-      .then(data => setStories(data))
+      .then(data => {
+        setStories(data)
+        setCategoryHead(data.section)
+        console.log('cateogry check in APP', category, stories)
+      })
   }
-
-
 
   return (
     <div className="App">
@@ -24,6 +32,7 @@ function App() {
         NY Times News Reader
       </header>
       <Search setFromSearch={setFromSearch}/>
+      <Headlines stories={stories} categoryHead={categoryHead}/>
     </div>
   );
 }
