034992840db95a645dfd0bb4ad403c2fca493d60
Rachel Jensen <rachelangela@gmail.com>
2021-11-05 13:49:17 -0600

    Add interfaces file

---
+++ src/interfaces.ts
@@ -0,0 +1,19 @@
+export interface IPropsToDV {
+  article: IArticle,
+  canClose: boolean,
+  showArticle: any,
+  setShowArticle: any
+}
+
+export interface IArticle {
+  created_date: string,
+  multimedia: IMultimedia[],
+  title: string,
+  byline: string,
+  url: string,
+  abstract: string
+}
+
+export interface IMultimedia {
+  url: string
+}
