a87a6c92333fe8a9040388e484a07124cd059520
Rachel Jensen <rachelangela@gmail.com>
2021-11-05 14:17:02 -0600

    Convert to tsx

--- src/components/Headlines.tsx
+++ src/components/Headlines.tsx
@@ -1,13 +1,16 @@
 import './Headlines.css';
+import { IPropsToHeadline } from '../interfaces';
 
-const Headlines = ({ stories, categoryHead, setArticle, setShowArticle }) => {
+
+
+const Headlines = ({ stories, categoryHead, setArticle, setShowArticle }: IPropsToHeadline) => {
   // use the stories from App to generate a list of headlines
   const headlines = stories.results.map((story, index) => {
     return (<button value={index} key={index} onClick={(event) => {handleDetails(event)}}>{story.title}</button>)
   })
 
   // make headlines links to the specific story
-  const handleDetails = (event) => {
+  const handleDetails = (event: any) => {
     //could I set the index as the value, then use the value to pull up the right story in the detailed view?
     event.preventDefault();
     setArticle(stories.results[event.target.value])
